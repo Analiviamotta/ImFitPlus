36e54ce4a3cd0349dd6cba65f68b19068134db7b
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-10-27 20:05:46 -0300

    refactor: use View Binding instead of setContentView with R.layout

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/ui/PersonalDataFormActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/ui/PersonalDataFormActivity.kt
@@ -16,6 +16,6 @@ class PersonalDataFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_personal_data_form)
+        setContentView(apdfb.root)
     }
 }
