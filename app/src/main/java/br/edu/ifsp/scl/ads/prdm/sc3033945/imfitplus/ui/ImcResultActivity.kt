272b0caa0d16b4b0c53dedb534557d5b1c8c770f
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-10-27 22:48:01 -0300

    styles: remove not used imports

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/ui/ImcResultActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/ui/ImcResultActivity.kt
@@ -1,11 +1,7 @@
 package br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.R
 import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.databinding.ActivityImcResultBinding
 
 class ImcResultActivity : AppCompatActivity() {
