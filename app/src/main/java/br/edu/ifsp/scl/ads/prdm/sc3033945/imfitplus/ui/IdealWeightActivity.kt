a57fa116977588d2822c474adb98b9e857e3b32b
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-10-28 00:23:33 -0300

    feat: use view binding in IdealWeightActivity

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/ui/IdealWeightActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/ui/IdealWeightActivity.kt
@@ -6,16 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.R
+import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.databinding.ActivityIdealWeightBinding
 
 class IdealWeightActivity : AppCompatActivity() {
+    private val aiwb: ActivityIdealWeightBinding by lazy {
+        ActivityIdealWeightBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_ideal_weight)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        setContentView(aiwb.root)
     }
 }
