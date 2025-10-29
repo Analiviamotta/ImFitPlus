e78de29d84519246dd51d6cc1692f2e2f1700ce8
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-10-29 01:25:38 -0300

    feat: calculate and exibity tmb info and daily calories

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/ui/DailyCaloriesActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/ui/DailyCaloriesActivity.kt
@@ -1,10 +1,15 @@
 package br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.ui
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.databinding.ActivityDailyCaloriesBinding
+import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model.ActivityLevel
+import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model.Constants.EXTRA_USER
+import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model.Gender
+import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model.UserDTO
 
 class DailyCaloriesActivity : AppCompatActivity() {
     private val adcb: ActivityDailyCaloriesBinding by lazy {
@@ -15,6 +20,21 @@ class DailyCaloriesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(adcb.root)
+
+        val user = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            intent.getParcelableExtra(EXTRA_USER, UserDTO::class.java)
+        }else {
+            intent.getParcelableExtra<UserDTO>(EXTRA_USER)
+        }
+
+        if (user == null) {
+            Toast.makeText(this, "Usuário não encontrado", Toast.LENGTH_SHORT).show()
+        } else {
+            val tmb = calculateTMB(user)
+            val dailyCalories = calculateDailyCalories(tmb, user.activityLevel)
+
+            adcb.dailyCaloriesResultTv.text = String.format("Seu gasto diário de calorias é %.0f kcal", dailyCalories)
+        }
 
         adcb.backBt.setOnClickListener {
             val message = "Voltando para tela anterior"
@@ -32,5 +52,33 @@ class DailyCaloriesActivity : AppCompatActivity() {
             }
         }
 
+
     }
+
+    private fun calculateFemaleTMB(user: UserDTO): Double {
+        return 655.1 + (9.6 * user.weight) + (1.8 * user.height * 100) - (4.7 * user.age)
+    }
+
+    private fun calculateMaleTMB(user: UserDTO): Double {
+        return 66.5 + (13.7 * user.weight) + (5.0 * user.height * 100) - (6.8 * user.age)
+    }
+
+    private fun calculateTMB(user: UserDTO): Double {
+        return if (user.gender == Gender.Female) {
+            calculateFemaleTMB(user)
+        } else {
+            calculateMaleTMB(user)
+        }
+    }
+
+    private fun calculateDailyCalories(tmb: Double, activityLevel: ActivityLevel): Double {
+        val multiplier = when(activityLevel) {
+            ActivityLevel.Sedentary -> 1.2
+            ActivityLevel.Light -> 1.375
+            ActivityLevel.Moderate -> 1.55
+            ActivityLevel.Intense -> 1.725
+        }
+        return tmb * multiplier
+    }
+
 }
