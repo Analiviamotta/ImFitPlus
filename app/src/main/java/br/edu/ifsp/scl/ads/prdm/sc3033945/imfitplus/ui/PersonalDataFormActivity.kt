7e2cf14b418deca66020d747f99bf7a1bd812db5
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-10-29 00:15:58 -0300

    feat: calculate imc

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/ui/PersonalDataFormActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/ui/PersonalDataFormActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.databinding.ActivityPersonalDataFormBinding
+import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model.Gender
 import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model.UserDTO
 
 class PersonalDataFormActivity : AppCompatActivity() {
@@ -19,11 +20,24 @@ class PersonalDataFormActivity : AppCompatActivity() {
         setContentView(apdfb.root)
 
         apdfb.calculateIMCBt.setOnClickListener {
-            val message = "Redirecionando para tela de IMC"
-             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
-            Intent(this, ImcResultActivity::class.java).let {
-                startActivity(it)
+            var height = apdfb.heightEt.text.toString().toDoubleOrNull() ?: 0.0
+            var weight = apdfb.weightEt.text.toString().toDoubleOrNull() ?: 0.0
+
+            if(height <= 0){
+                Toast.makeText(this, "Altura invalida", Toast.LENGTH_SHORT).show()
+            }else if (weight <= 0){
+                Toast.makeText(this, "Peso invalido", Toast.LENGTH_SHORT).show()
+            } else {
+
+                var imc = if (height > 0) weight / (height * height) else 0.0
+
+                val message = "Redirecionando para tela de IMC"
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+                Intent(this, ImcResultActivity::class.java).let {
+                    startActivity(it)
+                }
             }
         }
     }
