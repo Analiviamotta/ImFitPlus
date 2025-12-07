fa7066c03778870f8b9b4203c26f898515948fc7
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-12-06 23:43:10 -0300

    feat(history-activity): create history item DTO

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/model/Category.kt
@@ -0,0 +1,9 @@
+package br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model
+
+
+enum class Category(val label: String ) {
+    UNDERWEIGHT("Abaixo do peso"),
+    NORMAL("Normal"),
+    OVERWEIGHT("Sobrepeso"),
+    OBESITY("Obesidade");
+}
