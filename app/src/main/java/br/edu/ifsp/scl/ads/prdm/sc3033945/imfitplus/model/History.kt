dbc6de0cbe14495c0665c3c946602a975c2491f3
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-12-09 20:26:06 -0300

    feat(history): add history DTO

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/model/History.kt
@@ -0,0 +1,15 @@
+package br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model
+
+import java.time.LocalDateTime
+
+data class HistoryDTO(
+    var id: String = "",
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+    var userAge: Int = 0,
+    var height: Double = 0.0,
+    var weight: Double = 0.0,
+    var userActivityLevel: ActivityLevel = ActivityLevel.Sedentary,
+    var imc: Double = 0.0,
+    var idealWeight: Double = 0.0,
+    var category: Category
+)
