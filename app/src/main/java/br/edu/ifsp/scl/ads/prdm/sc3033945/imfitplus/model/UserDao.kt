8fcbd8c4670542a10b5fe10de01ad153ecf1f706
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-12-09 19:48:34 -0300

    feat(user-dao): create user dao interface

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/model/UserDao.kt
@@ -0,0 +1,8 @@
+package br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model
+
+interface UserDao {
+    fun createUser(user: UserDTO): Long
+    fun retrieveUser(userId: Int): UserDTO
+    fun retrieveUsers(): MutableList<UserDTO>
+    fun updateUser(user: UserDTO): Int
+}
