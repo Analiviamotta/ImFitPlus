8839c35872381708bbac0025ed2b1e673f4fab30
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-12-09 23:53:06 -0300

    refactor(enums): add value in enums

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/model/ActivityLevel.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/model/ActivityLevel.kt
@@ -1,8 +1,8 @@
 package br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model
 
-enum class ActivityLevel {
-    Sedentary,
-    Light,
-    Moderate,
-    Intense
+enum class ActivityLevel(val value: String) {
+    Sedentary("sedentary"),
+    Light("light"),
+    Moderate("moderate"),
+    Intense("intense")
 }
--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/model/Gender.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/model/Gender.kt
@@ -1,6 +1,6 @@
 package br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model
 
-enum class Gender {
-    Female,
-    Male
+enum class Gender(val value: String) {
+    Female("female"),
+    Male("male")
 }
