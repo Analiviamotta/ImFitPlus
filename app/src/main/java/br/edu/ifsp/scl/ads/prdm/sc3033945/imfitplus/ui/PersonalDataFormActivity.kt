ffc3fe7e020e3ad159cecd936b5fbad263914f82
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-12-10 20:34:41 -0300

    feat(personal-data-form): modified form to add pre set of infos

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/ui/PersonalDataFormActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/ui/PersonalDataFormActivity.kt
@@ -3,8 +3,10 @@ package br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.ui
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.controller.PersonalDataFormController
 import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.databinding.ActivityPersonalDataFormBinding
 import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model.ActivityLevel
 import br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model.Constants.EXTRA_IMC
@@ -19,10 +21,94 @@ class PersonalDataFormActivity : AppCompatActivity() {
         ActivityPersonalDataFormBinding.inflate(layoutInflater)
     }
 
+    private val userController : PersonalDataFormController by lazy {
+        PersonalDataFormController(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(apdfb.root)
+
+        apdfb.physicalActivityLevelSp.isEnabled = false
+
+        apdfb.isNotFirstUseCb.setOnCheckedChangeListener { _, isChecked ->
+
+            val enabled = !isChecked
+
+            apdfb.nameEt.isEnabled = enabled
+            apdfb.ageEt.isEnabled = enabled
+            apdfb.heightEt.isEnabled = enabled
+            apdfb.weightEt.isEnabled = enabled
+            apdfb.femaleRb.isEnabled = enabled
+            apdfb.maleRb.isEnabled = enabled
+
+
+            apdfb.physicalActivityLevelSp.isEnabled = enabled
+            if (enabled) {
+                apdfb.physicalActivityLevelSp.isEnabled = true
+            } else {
+                apdfb.physicalActivityLevelSp.isEnabled = false
+            }
+
+
+            apdfb.findUserBt.visibility = if (isChecked) View.VISIBLE else View.GONE
+        }
+
+        apdfb.findUserBt.setOnClickListener {
+            val name = apdfb.nameEt.text.toString().trim()
+
+            if (name.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Digite o nome para preencher as informações do usuário",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
+            val user = userController.getUserByName(name)
+
+
+            if (user.id.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Usuário não encontrado",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
+            apdfb.ageEt.setText(user.age.toString())
+            apdfb.heightEt.setText(user.height.toString())
+            apdfb.weightEt.setText(user.weight.toString())
+            apdfb.nameEt.setText(user.name)
+
+
+            if (user.gender == Gender.Female) {
+                apdfb.femaleRb.isChecked = true
+            } else {
+                apdfb.maleRb.isChecked = true
+            }
+
+
+            apdfb.physicalActivityLevelSp.setSelection(
+                when (user.activityLevel) {
+                    ActivityLevel.Sedentary -> 0
+                    ActivityLevel.Light -> 1
+                    ActivityLevel.Moderate -> 2
+                    ActivityLevel.Intense -> 3
+                }
+            )
+
+            Toast.makeText(
+                this,
+                "Usuário encontrado! Dados preenchidos.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+
 
         apdfb.calculateIMCBt.setOnClickListener {
 
