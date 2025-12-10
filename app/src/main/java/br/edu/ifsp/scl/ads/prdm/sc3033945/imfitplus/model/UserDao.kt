ddf1764625cbc92d1235ffb4de506e36dab8c932
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-12-09 23:51:32 -0300

    feat(user-dao): create retrieveUserByName in UserDao interface

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/model/UserDao.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/imfitplus/model/UserDao.kt
@@ -2,7 +2,8 @@ package br.edu.ifsp.scl.ads.prdm.sc3033945.imfitplus.model
 
 interface UserDao {
     fun createUser(user: UserDTO): Long
-    fun retrieveUser(userId: Int): UserDTO
+    fun retrieveUser(userId: String): UserDTO
+    fun retrieveUserByName(userName: String): UserDTO
     fun retrieveUsers(): MutableList<UserDTO>
     fun updateUser(user: UserDTO): Int
 }
